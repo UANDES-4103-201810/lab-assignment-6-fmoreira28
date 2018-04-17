9487be2cedfa4aabc5b5d0beb772dacfb4bee805
gtroncossi <gtroncossi@miuandes.cl>
2018-04-17 14:44:10 -0300

    Redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,19 @@ class RegistrationsController < ApplicationController
 	end
 
 	def create
+		@user = User.new(registrations_params)
+		if @user.save
+			flash[:notice]="User was created successful"
+			redirect_to root_path
+		else
+			flash[:notice]="User was not created successful"
+			redirect_to sign_in_path
+		end
+	end
 
+	private
+
+	def registrations_params
+		params.require(:registrations).permit(:name, :last_name, :email, :password, :phone)
 	end
 end
